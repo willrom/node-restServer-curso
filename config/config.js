2dcb613e73d0f4beb03f884b2d9012d826a7764c
Wilson Guerrero <willrom@hotmail.com>
2020-06-11 18:17:59 -0500

    Variable de entorno mongo

--- config/config.js
+++ config/config.js
@@ -13,7 +13,7 @@ let urlDB;
 if ( process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = 'mongodb+srv://willrom1128:Tejidos.1@cluster0-pzkay.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
